c262a69f996c2179302de33a5dd678ad6380e053
mohamed <elfirdoussimed99@gmail.com>
2022-04-05 07:56:23 +0000

    commiting last changes

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import SignupView, ProfileView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,74 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
+    <button v-if="!this.$store.state.isAuthenticated" @click="login()" type="button" >
+        <span>login</span>
+    </button>      
+    <button v-else @click="logout()">
+        <span> logout</span>
+    </button>
+
   <router-view/>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   beforeCreate(){
     this.$store.commit('initializeStorage')
   },
-}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  created(){
+    this.verifyAccess()
+  },
+  methods:{
+    async verifyAccess(){
+        const token = this.$store.state.token
+        await axios.post('/auth/token/verify/', {"token":token})
+        .then(response=>{
+            console.log('access is valid')
+            this.$store.state.isAuthenticated = true
+            this.$router.push('/')
+        })
+        .catch(err=>{
+            console.log(err)
+            console.log('access not valid')
+            this.$store.isAuthenticated = false
+        })
+    },
+    async login(){
+      const refresh = this.$store.state.refresh
+      if(typeof refresh === 'undefined'){
+        console.log('refresh undefined')
+        this.$router.push('/login')
+      }
+      else{
+        const data={
+          'refresh': refresh
+        }
+        console.log('inside else, refresh exists')
+        await axios.post('/auth/token/refresh/', data)
+        .then(response=>{
+          console.log(response.data)
+          const token = response.data.access
+          this.$store.commit('setToken', token) //setting the access token
+          this.$store.commit('setRefresh', refresh) //setting the refresh token
+          localStorage.setItem('token', token) //storing the access token to the localstorage, so that the user won't have to repeat everything if he/she reloads the page
+          localStorage.setItem('refresh', refresh) //storing the refresh token to the localstorage, so that the user won't have to repeat everything if he/she reloads the page
+          this.$store.isAuthenticated = true
+          this.$router.push('/')
+        })
+        .catch(err=>{
+          console.log(err)
+          this.$router.push('/login')
+        })
+      }
+    },
+    logout(){
+      localStorage.removeItem("token")
+      this.$store.commit('removeToken')
+      this.$router.push('/')
     }
   }
 }
-</style>
+</script>
+
+
